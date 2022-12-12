b125ece39c6bcc40b2d22bccddc8f6f6656b7991
Benjamin De Leener <benjamin.de-leener@polymtl.ca>
2022-12-12 10:03:03 -0500

    Initial commit

---
+++ tests.py
@@ -0,0 +1,47 @@
+import unittest
+from unittest import TestCase
+
+
+class TestFizzBuzz(TestCase):
+    def test_fizz_buzz_3(self):
+        # TODO Tester avec un multiple de 3
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+    def test_fizz_buzz_5(self):
+        # TODO Tester avec un multiple de 5
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+    def test_fizz_buzz_3_5(self):
+        # TODO Tester avec un multiple de 3 et 5
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+    def test_fizz_buzz_non_facteur(self):
+        # TODO Tester avec un nombre qui nombre'a pas 3 et 5 comme facteur
+        #  et assurez-vous que la valeur en sotie soit une string
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+
+class TestResoudreEquation(TestCase):
+    def test_resoudre_equation_sans_racine(self):
+        # TODO Tester avec un polynome sans racines réelles
+        #  et assurez-vous que la valeur en sortie est None
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+    def test_resoudre_equation_une_racine(self):
+        # TODO Tester avec un polynome avec une seule solution
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+    def test_resoudre_equation_deux_racine(self):
+        # TODO Tester avec un polynome avec deux solutions
+
+        self.fail("Retirez-moi")  # TODO retirez-moi
+
+
+if __name__ == '__main__':
+    unittest.main()
